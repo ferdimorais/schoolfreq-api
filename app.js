3b89b54373f2cb11c13606b9cc991322636b8492
ferdimorais <63595324+ferdimorais@users.noreply.github.com>
2024-04-17 18:29:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { notFound } from "./src/middlewares/notFound.js";
 import { handleError } from "./src/middlewares/handleError.js";
-import notesRoute from "./src/resources/notes/notes.routes.js";
+import alunoRoute from "./src/resources/aluno/aluno.routes.js";
+import faltaRoute from "./src/resources/falta/faltas.routes.js";
 dotenv.config();
 
 const app = express();
@@ -12,7 +13,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // api routes
-app.use("/notes", notesRoute);
+app.use("/aluno", alunoRoute);
+app.use("/falta", faltaRoute);
 
 app.use(notFound);
 app.use(handleError);
