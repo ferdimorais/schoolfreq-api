65644758c16d08ae15a389d23872e996579ca405
ferdimorais <63595324+ferdimorais@users.noreply.github.com>
2024-04-17 21:27:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-import { notFound } from "./src/middlewares/notFound.js";
-import { handleError } from "./src/middlewares/handleError.js";
-import alunoRoute from "./src/resources/aluno/aluno.routes.js";
-import faltaRoute from "./src/resources/falta/faltas.routes.js";
+import { notFound }     from "./src/middlewares/notFound.js";
+import { handleError }  from "./src/middlewares/handleError.js";
+import defaultRoute     from "./src/resources/default/default.routes.js";
+import alunoRoute       from "./src/resources/aluno/aluno.routes.js";
+import faltaRoute       from "./src/resources/falta/faltas.routes.js";
+import disciplinaRoute  from "./src/resources/disciplina/disciplina.routes.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//pagination
+const mongoosePaginate = require('mongoose-paginate');
+
 //middleware
 app.use(express.json());
 
 // api routes
+app.use("/", defaultRoute);
 app.use("/aluno", alunoRoute);
 app.use("/falta", faltaRoute);
+app.use("/disciplina", disciplinaRoute);
 
 app.use(notFound);
 app.use(handleError);
