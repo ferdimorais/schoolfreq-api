71d77759973a530e868cae65f13786c078c2ff8e
ferdimorais <63595324+ferdimorais@users.noreply.github.com>
2024-04-21 18:25:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // api routes
 app.use("/agenda",      agendaRoute);
 app.use("/aluno",       alunoRoute);
