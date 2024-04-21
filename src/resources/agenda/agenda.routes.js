cb41014920d5699ef3d1aaec4f025728b20665b2
ferdimorais <63595324+ferdimorais@users.noreply.github.com>
2024-04-21 15:58:17 -0300

    Update agenda.routes.js

--- src/resources/agenda/agenda.routes.js
+++ src/resources/agenda/agenda.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
+  getAllAgenda,
   getSingleAgenda,
   getAgendaProf,
 } from "./agenda.controllers.js";
 
 const router = express.Router();
 
+router.route("/").get(getAllAgenda);
 router.route("/:id").get(getSingleAgenda);
 router.route("/:data/:prof").get(getAgendaProf);
 
