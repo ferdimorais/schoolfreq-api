7b1048a9e6495914dead090aaaa5acfea0eb82e7
ferdimorais <63595324+ferdimorais@users.noreply.github.com>
2024-04-21 15:33:08 -0300

    Create agenda.controllers.js

---
+++ src/resources/agenda/agenda.controllers.js
@@ -0,0 +1,56 @@
+import { pool } from "../../db/connect.js";
+import { createCustomError } from "../../errors/customErrors.js";
+import { tryCatchWrapper } from "../../middlewares/tryCatchWrapper.js";
+
+/**
+ * @returns Agenda object
+ */
+async function getAgenda(id) {
+  let sql = "SELECT A.AGENDA, A.TURNO, A.DISCIPLINA, D.NOME_COMPL NOME_DISCIPLINA, A.TURMA, A.ANO, A.NUM_FUNC," +
+			"A.DATA, LEFT(RIGHT(A.HORA_INICIO,8),5) HORA_INICIO, LEFT(RIGHT(A.HORA_FIM,8),5) HORA_FIM FROM Agenda A " +
+			"INNER JOIN Disciplina D ON D.DISCIPLINA = A.DISCIPLINA WHERE A.AGENDA = ?";
+  const [rows] = await pool.query(sql, [id]);
+  return rows[0];
+}
+
+/**
+ * @description Get All Agenda
+ * @route GET /disciplina
+ */
+export const getAgendaProf = tryCatchWrapper(async function (req, res, next) {
+	const { data, prof } = req.body; // ou req.params, dependendo de onde você está recebendo esses valores
+  
+	const sql = `
+	  SELECT 
+	  		A.AGENDA, A.TURNO, A.DISCIPLINA, D.NOME_COMPL AS NOME_DISCIPLINA, A.TURMA, A.ANO, A.NUM_FUNC,
+			A.DATA, LEFT(RIGHT(A.HORA_INICIO,8),5) AS HORA_INICIO, LEFT(RIGHT(A.HORA_FIM,8),5) AS HORA_FIM 
+	  FROM 
+	  		Agenda A 
+	  		INNER JOIN Disciplina D ON D.DISCIPLINA = A.DISCIPLINA 
+	  WHERE 
+	  		A.DATA = :data AND A.NUM_FUNC = :prof 
+	  ORDER BY 
+	  		A.HORA_INICIO
+	`;
+  
+	const [rows] = await pool.query(sql, { data, prof });
+  
+	if (!rows.length) {
+	  return res.status(204).json({ message: "Não foram encontrados resultados" });
+	}
+  
+	return res.status(200).json({ Agenda: rows });
+});
+
+/**
+ * @description Get Single Agenda
+ * @route GET /disciplina/:id
+ */
+export const getSingleAgenda = tryCatchWrapper(async function (req, res, next) {
+	const { id } = req.params;
+  
+	const Agenda = await getAgenda(id);
+	if (!Agenda) return next(createCustomError("Agenda não encontrada", 404));
+  
+	return res.status(200).json(Agenda);
+  });
